826365606e8cc04f595d2332d7c1b9d9385a6e8f
danearboleda <nataarboleda2001@gmail.com>
2024-05-29 20:33:35 -0500

    Programa funcional

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,10 @@
-package test.java;
+import main.java.App;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
-    
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
 }
