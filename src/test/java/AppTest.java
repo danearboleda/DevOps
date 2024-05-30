3d9a1b42fd8f3c0319f52066c2319842d8ba298a
danearboleda <nataarboleda2001@gmail.com>
2024-05-29 20:52:30 -0500

    Prueba 3

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,5 +6,7 @@ public class AppTest {
     @Test
     public void testAdd() {
         assertEquals(10, App.add(7, 3));
+        assertEquals(99, App.add(11, 88));
+        assertEquals(50, App.add(16, 34));
     }
 }
